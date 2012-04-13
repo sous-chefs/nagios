68bbaa7fcd2178756dd9badcd981b62efc734e0b
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-13 12:21:35 -0700

    Make nginx bits dependencies instead of suggestions

--- metadata.rb
+++ metadata.rb
@@ -10,14 +10,10 @@ recipe "nagios::client", "Installs and configures a nagios client with nrpe"
 recipe "nagios::server", "Installs and configures a nagios server"
 recipe "nagios::pagerduty", "Integrates contacts w/ PagerDuty API"
 
-%w{ apache2 build-essential php}.each do |cb|
+%w{ apache2 build-essential php nginx nginx_simplecgi }.each do |cb|
   depends cb
 end
 
 %w{ debian ubuntu redhat centos fedora scientific}.each do |os|
   supports os
-end
-
-%w{ nginx nginx_simplecgi }.each do |pkg|
-  suggests pkg
 end
