4ae3e5eda738b5149e1853273047b842a7a18f4d
jblatt-verticoud <jblatt@verticloud.com>
2012-08-08 10:32:44 -0700

    Added support for chained CAs for our wildcard certs.   Fixed a bug in the init.d script (typo in pid file, try to kill service if it exit non-zero but continues to run)

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -7,18 +7,18 @@ when "openid"
   include_recipe "apache2::mod_auth_openid"
 else
   sysadmins = node['nagios']['sysadmins'] || search(:users, 'groups:sysadmin')
-  
+
   dir = node['nagios']['conf_dir']
   owner = node['nagios']['user']
   group = node['apache']['user']
-  
+
   directory dir do
     owner owner
     group group
     mode 00750
     action :create
   end
-      
+
   template "#{dir}/htpasswd.users" do
     source "htpasswd.users.erb"
     owner owner
@@ -40,10 +40,20 @@ else
   public_domain = node['domain']
 end
 
+ssl_certificate_file = node['nagios']['ssl_certificate_file'] ||
+  "#{node['nagios']['conf_dir']}/certificates/nagios-server.pem"
+ssl_certificate_key_file = node['nagios']['ssl_certificate_key_file'] ||
+  "#{node['nagios']['conf_dir']}/certificates/nagios-server.pem"
+
 template "#{node['apache']['dir']}/sites-available/nagios3.conf" do
   source "apache2.conf.erb"
   mode 00644
-  variables :public_domain => public_domain
+  variables(
+      :ssl_certificate_file => ssl_certificate_file,
+      :ssl_certificate_key_file => ssl_certificate_key_file,
+      :ssl_certificate_chain_file => node['nagios']['ssl_certificate_chain_file'],
+      :public_domain => public_domain
+  )
   if ::File.symlink?("#{node['apache']['dir']}/sites-enabled/nagios3.conf")
     notifies :reload, "service[apache2]"
   end
