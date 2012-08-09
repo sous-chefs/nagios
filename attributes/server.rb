10f9d5f9ea1eb4e6761324a4e3b4acc142c12189
Tim Smith <tim.smith@webtrends.com>
2012-08-09 16:19:53 -0700

    Add flap_detection attribute and set it to the current default

--- attributes/server.rb
+++ attributes/server.rb
@@ -76,11 +76,13 @@ default['nagios']['default_host']['check_interval']     = 15
 default['nagios']['default_host']['retry_interval']     = 15
 default['nagios']['default_host']['max_check_attempts'] = 1
 default['nagios']['default_host']['notification_interval'] = 300
+default['nagios']['default_host']['flap_detection'] = true
 
 default['nagios']['default_service']['check_interval']     = 60
 default['nagios']['default_service']['retry_interval']     = 15
 default['nagios']['default_service']['max_check_attempts'] = 3
 default['nagios']['default_service']['notification_interval'] = 1200
+default['nagios']['default_service']['flap_detection'] = true
 
 default['nagios']['server']['web_server'] = :apache
 default['nagios']['server']['nginx_dispatch'] = :cgi
