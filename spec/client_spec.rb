826ee37f3e98ab847014138efb41c633e41c8440
Anuj Biyani <abiyani@lytro.com>
2013-02-27 15:39:16 -0800

    change a spec so that it ensures a search with no results does not raise an error

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -15,9 +15,9 @@ describe 'nagios::client' do
     expect(chef_run).to create_file_with_content "#{chef_run.node['nagios']['nrpe']['conf_dir']}/nrpe.cfg", 'allowed_hosts=127.0.0.1,test.host'
   end
 
-  it 'does not blow up when allowed hosts is not defined' do
-    Chef::Recipe.any_instance.stub(:search)
+  it 'does not blow up when the search returns no results' do
+    Chef::REST.any_instance.stub(:get_rest).and_return({"rows"=>[], "start"=>0, "total"=>0})
 
-    expect(chef_run).to create_file_with_content "#{chef_run.node['nagios']['nrpe']['conf_dir']}/nrpe.cfg", 'allowed_hosts=127.0.0.1'
+    lambda { chef_run }.should_not raise_error
   end
 end
