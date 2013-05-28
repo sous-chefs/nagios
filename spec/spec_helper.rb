ea7f1fe0f9d871fe227518a58ca282730ae400b1
jtimberman <joshua@opscode.com>
2013-05-28 09:51:01 -0600

    Don't use Ruby 1.9 hash syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ def runner(attributes = {}, environment = 'test')
   cookbook_paths = %W(#{File.expand_path("..", Dir.pwd)} #{File.expand_path(Dir.pwd)}/cookbooks)
 
   # A workaround so that ChefSpec can work with Chef environments (from https://github.com/acrmp/chefspec/issues/54)
-  @runner ||= ChefSpec::ChefRunner.new(cookbook_path: cookbook_paths, platform: 'ubuntu', version: '10.04') do |node|
+  @runner ||= ChefSpec::ChefRunner.new(:cookbook_path => cookbook_paths, :platform => 'ubuntu', :version => '10.04') do |node|
     env = Chef::Environment.new
     env.name environment
     node.stub(:chef_environment).and_return env.name
