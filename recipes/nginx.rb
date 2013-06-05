677a73aa4294fc8ac5dd6b1b8c2224c13bddfb7a
Tim Smith <tsmith@llnw.com>
2013-06-05 09:38:59 -0700

    Simplify detection of RHEL 5 and fix lack of support for RHEL 5.9 (current release)
    Replicate previous logic where all RHEL versions set
    node['nagios']['server']['nginx_dispatch'] = :both and versions less
    than 6 installed via source.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -20,20 +20,14 @@ if node["nagios"]["server"]["stop_apache"]
   end
 end
 
-via_pkg = value_for_platform_family(
-  %w(rhel fedora) => {
-    %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => false,
-    "default" => nil
-  },
-  "default" => true
-)
-
-if(via_pkg.nil?)
+# This doesn't use value_for_platform_family so that it can specify version ranges - COOK-2891
+if platform_family?('rhel') || platform_family?('fedora')
   node.set['nagios']['server']['nginx_dispatch'] = :both
-elsif(via_pkg == false)
-  node.set["nginx"]["install_method"] = 'source'
-  node.set['nagios']['server']['nginx_dispatch'] = :both
+  if node['platform_version'].to_f < 6
+    node.set['nginx']['install_method'] = 'source'
+  end
 end
+
 include_recipe "nginx"
 
 %w(default 000-default).each do |disable_site|
@@ -92,4 +86,3 @@ end
 nginx_site "nagios3.conf" do
   notifies :reload, "service[nginx]"
 end
-
