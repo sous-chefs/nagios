af0fb0835cdb151ebeb1453ad40c164fa034296d
pierre-o13 <pierre.ozoux@seedrs.com>
2013-06-07 17:51:04 +0100

    Correct the issue - COOK-3136
    http://tickets.opscode.com/browse/COOK-3136

--- recipes/client_source.rb
+++ recipes/client_source.rb
@@ -23,6 +23,7 @@ include_recipe "build-essential"
 pkgs = value_for_platform_family(
     ["rhel","fedora"] => ["openssl-devel","make","tar"] ,
     "debian" => ["libssl-dev","make","tar"],
+    "gentoo" => [],
     "default" => ["libssl-dev","make","tar"]
   )
 
