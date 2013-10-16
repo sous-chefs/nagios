9f48421cf9e0d6786edc35de85b62430587b4fd5
jaxjacobs <jack.jacobs@tealium.com>
2013-10-16 16:03:57 -0700

    Added functinality to check_rabbit_unackd to list all queues with issues in the alert.

--- files/default/plugins/check_rabbitmq_unackd.py
+++ files/default/plugins/check_rabbitmq_unackd.py
@@ -20,11 +20,14 @@ WARNACK = int(options.warnack)
 GET_VHOSTS = "%s -q list_vhosts" % (RABBITMQCTL)
 vhostdata = os.popen(GET_VHOSTS).read().strip()
 
+ALERT_CRIT = []
+ALERT_WARN = []
+
 for vhost in vhostdata.split("\n"):
     if vhostdata is not None:
 
         UNAKD = ""
-        UNAKD = "%s -q list_queues -p %s %s" % (RABBITMQCTL, vhost, options.info)
+        UNAKD = "%s -q list_queues -p %s %s %s" % (RABBITMQCTL, vhost, options.info, NAME)
 
         if not os.path.exists(RABBITMQCTL):
             print "%s does not exist" % RABBITMQCTL
@@ -43,9 +46,10 @@ for vhost in vhostdata.split("\n"):
         perf = ""
 
         def check_line2(result, queue):
+
             result = int(result)
             globals()['output'] = "%s" % (result)
-            
+
             globals()['perf'] += "unacknowledged=%sITEMS;%s;%s;; " % (result, WARNACK, CRITACK)
             if result > CRITACK:
                 globals()['is_critack'] = True
@@ -54,12 +58,14 @@ for vhost in vhostdata.split("\n"):
                     globals()['is_warnack'] = True
 
                 if is_critack is True:
-                    sys.stdout.write("UNAKD MESSAGE CRITICAL FOR VHOST %s and QUEUE %s - %s|%s\n" % (vhost, queue, output, perf))
-                    sys.exit(2)
+                    MSG = "UNAKD MESSAGE CRITICAL FOR VHOST %s and QUEUE %s - %s\n" % (vhost, queue, output)
+                    ALERT_CRIT.append(MSG)
+                    globals()['is_critack'] = False
 
                 if is_warnack is True:
-                    sys.stdout.write("UNAKD MESSAGE WARNING FOR VHOST %s and QUEUE %s - %s|%s\n" % (vhost, queue, output, perf))
-                    sys.exit(1)
+                    MSG = "UNAKD MESSAGE WARNING FOR VHOST %s and QUEUE %s - %s\n" % (vhost, queue, output)
+                    ALERT_WARN.append(MSG)
+                    globals()['is_warnack'] = False
 
         for results in unakd.split("\n"):
             if not results:
@@ -73,6 +79,18 @@ for vhost in vhostdata.split("\n"):
     else:
         sys.stdout.write("There seems to be no VHOSTS on this machine.\n")
         sys.exit(2)
+
+if ALERT_CRIT:
+    for alerts in ALERT_CRIT:
+        sys.stdout.write("%s\n" % (alerts))
+    for alerts in ALERT_WARN:
+        sys.stdout.write("%s\n" % (alerts))
+    sys.exit(2)
+
+if ALERT_WARN:
+    for alerts in ALERT_WARN:
+        sys.stdout.write("%s\n" % (alerts))
+    sys.exit(1)
 
 sys.stdout.write("UNAKD MESSAGE OK - %s|%s\n" % (output, perf))
 sys.exit(0)
