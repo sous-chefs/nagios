5a230a9be856587ad07ecd191937944b14d34a61
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2013-11-25 22:39:46 +0000

    add minimal server spec

---
+++ spec/server_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'nagios::server' do
+  let(:chef_run) { runner.converge 'nagios::server' }
+  subject { chef_run }
+  before do
+    stub_search(:users, 'groups:sysadmin NOT action:remove').
+      and_return([])
+
+    stub_search(:node, 'hostname:* AND chef_environment:test').
+      and_return([])
+
+    stub_search(:role, '*:*').
+      and_return([])
+
+    Chef::DataBag.
+      stub(:list).
+      and_return([])
+
+   # nagios::server_package stubs
+   stub_command('dpkg -l nagios3').and_return(true)
+  end
+  it { should install_package 'nagios3' }
+  it { should enable_service 'nagios' }
+  it { should start_service 'nagios' }
+end
+
