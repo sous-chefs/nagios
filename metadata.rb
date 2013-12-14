0ad283468c000bba56191d8a5a7995df7c5cfcda
Tim Smith <tsmith84@gmail.com>
2013-12-14 15:07:43 -0800

    Pin yum dependency below version 3 to not break EPEL functionality

--- metadata.rb
+++ metadata.rb
@@ -11,9 +11,10 @@ recipe 'nagios::client', 'Installs and configures a nrpe client'
 recipe 'nagios::server', 'Installs and configures a nagios server'
 recipe 'nagios::pagerduty', 'Integrates contacts w/ PagerDuty API'
 
-%w{ apache2 build-essential php nginx nginx_simplecgi yum }.each do |cb|
+%w{ apache2 build-essential php nginx nginx_simplecgi }.each do |cb|
   depends cb
 end
+depends 'yum', '< 3.0'
 
 %w{ debian ubuntu redhat centos fedora scientific amazon oracle}.each do |os|
   supports os
