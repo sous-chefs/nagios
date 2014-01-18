46e0bd82005b2c2b51978690b33000b8a1d37eb6
Tim Smith <tsmith@llnw.com>
2014-01-18 14:03:35 -0800

    Fix the search mock in for chef spec.  Also fix indentation in the server_spec file

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -6,14 +6,14 @@ describe 'nagios::server' do
   before do
     stub_search(:users, 'groups:sysadmin NOT action:remove').and_return([])
 
-    stub_search(:node, 'hostname:* AND chef_environment:test').and_return([])
+    stub_search(:node, 'name:* AND chef_environment:test').and_return([])
 
     stub_search(:role, '*:*').and_return([])
 
     Chef::DataBag.stub(:list).and_return([])
 
-   # nagios::server_package stubs
-   stub_command('dpkg -l nagios3').and_return(true)
+    # nagios::server_package stubs
+    stub_command('dpkg -l nagios3').and_return(true)
   end
   it { should install_package 'nagios3' }
   it { should enable_service 'nagios' }
