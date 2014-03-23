b9b5536207e6ab7db7b467caa60281d9655bb25c
Tim Smith <tsmith@llnw.com>
2014-03-23 12:08:33 -0700

    Switch to chef-zero in the specs so less has to be mocked out.

---
+++ spec/package_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'nagios::default' do
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04')
+    runner.converge 'nagios::default'
+  end
+  subject { chef_run }
+  before do
+    ChefSpec::Server.create_data_bag('users', {
+      'tsmith' => {
+        'group' => 'sysadmin'
+      },
+      'bsmith' => {
+        'group' => 'users'
+      }
+    })
+
+    stub_command('dpkg -l nagios3').and_return(true)
+  end
+
+  it 'should install correction packages' do
+    expect(chef_run).to install_package 'nagios3'
+  end
+
+  it 'should start and enable service nagios' do
+    expect(chef_run).to start_service 'nagios'
+    expect(chef_run).to enable_service 'nagios'
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,5 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'chefspec/server'
 
-def runner(attributes = {}, environment = 'test')
-  # A workaround so that ChefSpec can work with Chef environments (from https://github.com/acrmp/chefspec/issues/54)
-  @runner ||= ChefSpec::Runner.new(:platform => 'ubuntu', :version => '12.04') do |node|
-    env = Chef::Environment.new
-    env.name environment
-    node.stub(:chef_environment).and_return env.name
-    Chef::Environment.stub(:load).and_return env
-
-    attributes.each_pair do |key, val|
-      node.set[key] = val
-    end
-  end
-end
+at_exit { ChefSpec::Coverage.report! }
