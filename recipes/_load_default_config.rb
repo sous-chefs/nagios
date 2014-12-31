c01fe7a748cff5d071df9a46684d7bbc996b90bd
Sander Botman <sander.botman@gmail.com>
2014-12-31 10:50:20 +0100

    moving the default config into lwrp's this makes it more chef-like and better to read.

--- recipes/_load_default_config.rb
+++ recipes/_load_default_config.rb
@@ -41,168 +41,178 @@ end
 nodes.each { |n| Nagios.instance.push(n) }
 
 # 24x7 timeperiod
-t = Nagios::Timeperiod.create('24x7')
-t.alias = '24 Hours A Day, 7 Days A Week'
-%w( sunday
-    monday
-    tuesday
-    wednesday
-    thursday
-    friday
-    saturday
-).each do |day|
-  t.push(Nagios::Timeperiodentry.new(day, '00:00-24:00'))
+nagios_timeperiod '24x7' do
+  options 'alias' => '24 Hours A Day, 7 Days A Week',
+          'times' => { 'sunday'    => '00:00-24:00',
+                       'monday'    => '00:00-24:00',
+                       'tuesday'   => '00:00-24:00',
+                       'wednesday' => '00:00-24:00',
+                       'thursday'  => '00:00-24:00',
+                       'friday'    => '00:00-24:00',
+                       'saturday'  => '00:00-24:00'
+                     }
 end
 
 # Host checks
-check_host_alive = Nagios::Command.create('check_host_alive')
-check_host_alive.command_line = '$USER1$/check_ping -H $HOSTADDRESS$ -w 2000,80% -c 3000,100% -p 1'
+nagios_command 'check_host_alive' do
+  options  'command_line' => '$USER1$/check_ping -H $HOSTADDRESS$ -w 2000,80% -c 3000,100% -p 1'
+end
 
 # Service checks
-check_nagios = Nagios::Command.create('check_nagios')
-check_nagios.command_line = '$USER1$/check_nrpe -H $HOSTADDRESS$ -c check_nagios -t 20'
+nagios_command 'check_nagios' do
+  options 'command_line' => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c check_nagios -t 20'
+end
 
 # nrpe remote host checks
-check_nrpe_alive = Nagios::Command.create('check_nrpe_alive')
-check_nrpe_alive.command_line = '$USER1$/check_nrpe -H $HOSTADDRESS$ -t 20'
+nagios_command 'check_nrpe_alive' do
+  options 'command_line' => '$USER1$/check_nrpe -H $HOSTADDRESS$ -t 20'
+end
 
-check_nrpe = Nagios::Command.create('check_nrpe')
-check_nrpe.command_line = '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -t 20'
+nagios_command 'check_nrpe' do
+  options 'command_line' => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -t 20'
+end
 
 # host_notify_by_email command
-host_notify_by_email = Nagios::Command.create('host_notify_by_email')
-host_notify_by_email.command_line = '/usr/bin/printf "%b" "$LONGDATETIME$\n\n$HOSTALIAS$ $NOTIFICATIONTYPE$ $HOSTSTATE$\n\n$HOSTOUTPUT$\n\nLogin: ssh://$HOSTNAME$" | ' + node['nagios']['server']['mail_command'] + ' -s "$NOTIFICATIONTYPE$ - $HOSTALIAS$ $HOSTSTATE$!" $CONTACTEMAIL$'
+nagios_command 'host_notify_by_email' do
+  options 'command_line' => '/usr/bin/printf "%b" "$LONGDATETIME$\n\n$HOSTALIAS$ $NOTIFICATIONTYPE$ $HOSTSTATE$\n\n$HOSTOUTPUT$\n\nLogin: ssh://$HOSTNAME$" | ' + node['nagios']['server']['mail_command'] + ' -s "$NOTIFICATIONTYPE$ - $HOSTALIAS$ $HOSTSTATE$!" $CONTACTEMAIL$'
+end
 
 # service_notify_by_email command
-service_notify_by_email = Nagios::Command.create('service_notify_by_email')
-service_notify_by_email.command_line = '/usr/bin/printf "%b" "$LONGDATETIME$ - $SERVICEDESC$ $SERVICESTATE$\n\n$HOSTALIAS$  $NOTIFICATIONTYPE$\n\n$SERVICEOUTPUT$\n\nLogin: ssh://$HOSTNAME$" | ' + node['nagios']['server']['mail_command'] + ' -s "** $NOTIFICATIONTYPE$ - $HOSTALIAS$ - $SERVICEDESC$ - $SERVICESTATE$" $CONTACTEMAIL$'
+nagios_command 'service_notify_by_email' do
+  options 'command_line' => '/usr/bin/printf "%b" "$LONGDATETIME$ - $SERVICEDESC$ $SERVICESTATE$\n\n$HOSTALIAS$  $NOTIFICATIONTYPE$\n\n$SERVICEOUTPUT$\n\nLogin: ssh://$HOSTNAME$" | ' + node['nagios']['server']['mail_command'] + ' -s "** $NOTIFICATIONTYPE$ - $HOSTALIAS$ - $SERVICEDESC$ - $SERVICESTATE$" $CONTACTEMAIL$'
+end
 
 # host_notify_by_sms_email command
-host_notify_by_sms_email = Nagios::Command.create('host_notify_by_sms_email')
-host_notify_by_sms_email.command_line = '/usr/bin/printf "%b" "$HOSTALIAS$ $NOTIFICATIONTYPE$ $HOSTSTATE$\n\n$HOSTOUTPUT$" | ' + node['nagios']['server']['mail_command'] + ' -s "$HOSTALIAS$ $HOSTSTATE$!" $CONTACTPAGER$'
+nagios_command 'host_notify_by_sms_email' do
+  options 'command_line' => '/usr/bin/printf "%b" "$HOSTALIAS$ $NOTIFICATIONTYPE$ $HOSTSTATE$\n\n$HOSTOUTPUT$" | ' + node['nagios']['server']['mail_command'] + ' -s "$HOSTALIAS$ $HOSTSTATE$!" $CONTACTPAGER$'
+end
 
 # root contact
-c = Nagios::Contact.create('root')
-options = { 
-  'alias'                         => 'Root',
-  'service_notification_period'   => t,
-  'host_notification_period'      => t,
-  'service_notification_options'  => 'w,u,c,r',
-  'host_notification_options'     => 'd,r',
-  'service_notification_commands' => service_notify_by_email,
-  'host_notification_commands'    => host_notify_by_email,
-  'email'                         => 'root@localhost'
-}
-c.import(options)
+nagios_contact 'root' do
+  options 'alias'                         => 'Root',
+          'service_notification_period'   => '24x7',
+          'host_notification_period'      => '24x7',
+          'service_notification_options'  => 'w,u,c,r',
+          'host_notification_options'     => 'd,r',
+          'service_notification_commands' => 'service_notify_by_email',
+          'host_notification_commands'    => 'host_notify_by_email',
+          'email'                         => 'root@localhost'
+end
 
 # admin contact
-c = Nagios::Contact.create('admin')
-c.alias                         = 'Admin'
-c.service_notification_period   = t
-c.host_notification_period      = t
-c.service_notification_options  = 'w,u,c,r'
-c.host_notification_options     = 'd,r'
-c.service_notification_commands = service_notify_by_email
-c.host_notification_commands    = host_notify_by_email
+nagios_contact 'admin' do
+  options 'alias'                         => 'Admin',
+          'service_notification_period'   => '24x7',
+          'host_notification_period'      => '24x7',
+          'service_notification_options'  => 'w,u,c,r',
+          'host_notification_options'     => 'd,r',
+          'service_notification_commands' => 'service_notify_by_email',
+          'host_notification_commands'    => 'host_notify_by_email'
+end 
 
-c = Nagios::Contact.create('default-contact')
-c.name                            = 'default-contact'
-c.service_notification_period     = t
-c.host_notification_period        = t
-c.service_notification_options    = 'w,u,c,r,f'
-c.host_notification_options       = 'd,u,r,f,s'
-c.service_notification_commands   = service_notify_by_email
-c.host_notification_commands      = host_notify_by_email
+nagios_contact 'default-contact' do
+  options 'name'                            => 'default-contact',
+          'service_notification_period'     => '24x7',
+          'host_notification_period'        => '24x7',
+          'service_notification_options'    => 'w,u,c,r,f',
+          'host_notification_options'       => 'd,u,r,f,s',
+          'service_notification_commands'   => 'service_notify_by_email',
+          'host_notification_commands'      => 'host_notify_by_email'
+end
 
 # This was taken from the origional cookbook, but cannot find the
 # commands service-notify-by-sms-gateway and host-notify-by-sms-gateway
 # anywhere defined, so skipping this here.
 #
-# c = Nagios::Contact.create('sms-contact')
-# c.name                            = 'sms-contact'
-# c.service_notification_period     = t
-# c.host_notification_period        = t
-# c.service_notification_options    = 'w,u,c,r,f'
-# c.host_notification_options       = 'd,u,r,f,s'
-# c.service_notification_commands   = service-notify-by-sms-gateway
-# c.host_notification_commands      = host-notify-by-sms-gateway
+# nagios_contact 'sms-contact' do
+#   options 'name'                          => 'sms-contact',
+#           'service_notification_period'   => '24x7',
+#           'host_notification_period'      => '24x7',
+#           'service_notification_options'  => 'w,u,c,r,f',
+#           'host_notification_options'     => 'd,u,r,f,s',
+#           'service_notification_commands' => 'service-notify-by-sms-gateway',
+#           'host_notification_commands'    => 'host-notify-by-sms-gateway'
+# end
 
-h = Nagios::Host.create('default-host')
-h.name                         = 'default-host'
-h.notifications_enabled        = 1
-h.event_handler_enabled        = 1
-h.flap_detection_enabled       = nagios_boolean(nagios_attr(:default_host)[:flap_detection])
-h.process_perf_data            = nagios_boolean(nagios_attr(:default_host)[:process_perf_data])
-h.retain_status_information    = 1
-h.retain_nonstatus_information = 1
-h.notification_period          = t
-h.register                     = 0
-h.action_url                   = nagios_attr(:default_host)[:action_url]
+nagios_host 'default-host' do
+  options 'name'                         => 'default-host',
+          'notifications_enabled'        => 1,
+          'event_handler_enabled'        => 1,
+          'flap_detection_enabled'       => nagios_boolean(nagios_attr(:default_host)[:flap_detection]),
+          'process_perf_data'            => nagios_boolean(nagios_attr(:default_host)[:process_perf_data]),
+          'retain_status_information'    => 1,
+          'retain_nonstatus_information' => 1,
+          'notification_period'          => '24x7',
+          'register'                     => 0,
+          'action_url'                   => nagios_attr(:default_host)[:action_url]
+end
 
-h = Nagios::Host.create('server')
-h.name                    = 'server'
-h.use                     = 'default-host'
-h.check_period            = nagios_attr(:default_host)[:check_period]
-h.check_interval          = nagios_interval(nagios_attr(:default_host)[:check_interval])
-h.retry_interval          = nagios_interval(nagios_attr(:default_host)[:retry_interval])
-h.max_check_attempts      = nagios_attr(:default_host)[:max_check_attempts]
-h.check_command           = nagios_attr(:default_host)[:check_command]
-h.notification_interval   = nagios_interval(nagios_attr(:default_host)[:notification_interval])
-h.notification_options    = nagios_attr(:default_host)[:notification_options]
-nagios_attr(:default_contact_groups).each {|c| h.push(Nagios::Contactgroup.create(c)) }
-h.register                = 0
+nagios_host 'server' do
+  options 'name'                   => 'server',
+          'use'                    => 'default-host',
+          'check_period'           => nagios_attr(:default_host)[:check_period],
+          'check_interval'         => nagios_interval(nagios_attr(:default_host)[:check_interval]),
+          'retry_interval'         => nagios_interval(nagios_attr(:default_host)[:retry_interval]),
+          'max_check_attempts'     => nagios_attr(:default_host)[:max_check_attempts],
+          'check_command'          => nagios_attr(:default_host)[:check_command],
+          'notification_interval'  => nagios_interval(nagios_attr(:default_host)[:notification_interval]),
+          'notification_options'   => nagios_attr(:default_host)[:notification_options],
+          'default_contact_groups' => nagios_attr(:default_contact_groups),
+          'register'               => 0
+end
 
 # Defaut host template
-Nagios.instance.default_host = h
+Nagios.instance.default_host = 'server'
 
 #Services
-s = Nagios::Service.create('default-service')
-s.name                            = 'default-service'
-s.active_checks_enabled           = 1
-s.passive_checks_enabled          = 1
-s.parallelize_check               = 1
-s.obsess_over_service             = 1
-s.check_freshness                 = 0
-s.notifications_enabled           = 1
-s.event_handler_enabled           = 1
-s.flap_detection_enabled          = nagios_boolean(nagios_attr(:default_service)[:flap_detection])
-s.process_perf_data               = nagios_boolean(nagios_attr(:default_service)[:process_perf_data])
-s.retain_status_information       = 1
-s.retain_nonstatus_information    = 1
-s.is_volatile                     = 0
-s.check_period                    = t
-s.max_check_attempts              = nagios_attr(:default_service)[:max_check_attempts]
-s.check_interval                  = nagios_interval(nagios_attr(:default_service)[:check_interval])
-s.retry_interval                  = nagios_interval(nagios_attr(:default_service)[:retry_interval])
-nagios_attr(:default_contact_groups).each {|c| s.push(Nagios::Contactgroup.create(c)) }
-s.notification_options            = 'w,u,c,r'
-s.notification_interval           = nagios_interval(nagios_attr(:default_service)[:notification_interval])
-s.notification_period             = t
-s.register                        = 0
-s.action_url                      = nagios_attr(:default_service)[:action_url]
-
+nagios_service 'default-service' do
+  options 'name'                         => 'default-service',
+          'active_checks_enabled'        => 1,
+          'passive_checks_enabled'       => 1,
+          'parallelize_check'            => 1,
+          'obsess_over_service'          => 1,
+          'check_freshness'              => 0,
+          'notifications_enabled'        => 1,
+          'event_handler_enabled'        => 1,
+          'flap_detection_enabled'       => nagios_boolean(nagios_attr(:default_service)[:flap_detection]),
+          'process_perf_data'            => nagios_boolean(nagios_attr(:default_service)[:process_perf_data]),
+          'retain_status_information'    => 1,
+          'retain_nonstatus_information' => 1,
+          'is_volatile'                  => 0,
+          'check_period'                 => '24x7',
+          'max_check_attempts'           => nagios_attr(:default_service)[:max_check_attempts],
+          'check_interval'               => nagios_interval(nagios_attr(:default_service)[:check_interval]),
+          'retry_interval'               => nagios_interval(nagios_attr(:default_service)[:retry_interval]),
+          'default_contact_groups'       => nagios_attr(:default_contact_groups),
+          'notification_options'         => 'w,u,c,r',
+          'notification_interval'        => nagios_interval(nagios_attr(:default_service)[:notification_interval]),
+          'notification_period'          => '24x7',
+          'register'                     => 0,
+          'action_url'                   => nagios_attr(:default_service)[:action_url]
+end
 # Default service template
-Nagios.instance.default_service = s
+Nagios.instance.default_service = 'default-service'
 
 # Define the log monitoring template (monitoring logs is very different)
-s = Nagios::Service.create('default-logfile')
-s.name                            = 'default-logfile'
-s.use                             = 'default-service'
-s.check_period                    = t
-s.max_check_attempts              = 1
-s.check_interval                  = nagios_interval(nagios_attr(:default_service)[:check_interval])
-s.retry_interval                  = nagios_interval(nagios_attr(:default_service)[:retry_interval])
-nagios_attr(:default_contact_groups).each {|c| s.push(Nagios::Contactgroup.create(c)) }
-s.notification_options            = 'w,u,c,r'
-s.notification_period             = t
-s.register                        = 0
-s.is_volatile                     = 1
+nagios_service 'default-logfile' do
+  options 'name'                   => 'default-logfile',
+          'use'                    => 'default-service',
+          'check_period'           => '24x7',
+          'max_check_attempts'     => 1,
+          'check_interval'         => nagios_interval(nagios_attr(:default_service)[:check_interval]),
+          'retry_interval'         => nagios_interval(nagios_attr(:default_service)[:retry_interval]),
+          'default_contact_groups' => nagios_attr(:default_contact_groups),
+          'notification_options'   => 'w,u,c,r',
+          'notification_period'    => '24x7',
+          'register'               => 0,
+          'is_volatile'            => 1
+end
 
-s = Nagios::Service.create('service-template')
-s.name                            = 'service-template'
-s.max_check_attempts              = nagios_attr(:default_service)[:max_check_attempts]
-s.check_interval                  = nagios_interval(nagios_attr(:default_service)[:check_interval])
-s.retry_interval                  = nagios_interval(nagios_attr(:default_service)[:retry_interval])
-s.notification_interval           = nagios_interval(nagios_attr(:default_service)[:notification_interval])
-s.register                        = 0
-
+nagios_service 'service-template' do
+  options 'name'                  => 'service-template',
+          'max_check_attempts'    => nagios_attr(:default_service)[:max_check_attempts],
+          'check_interval'        => nagios_interval(nagios_attr(:default_service)[:check_interval]),
+          'retry_interval'        => nagios_interval(nagios_attr(:default_service)[:retry_interval]),
+          'notification_interval' => nagios_interval(nagios_attr(:default_service)[:notification_interval]),
+          'register'              => 0
+end
