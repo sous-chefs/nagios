3190c1e144b1452406d25be980fa5c15fa8fed6b
Sander Botman <sbotman@schubergphilis.com>
2015-08-14 17:08:20 +0200

    deploying new version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'tsmith84@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures Nagios server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '7.1.7'
+version           '7.1.8'
 
 recipe 'default', 'Installs Nagios server.'
 recipe 'nagios::pagerduty', 'Integrates contacts w/ PagerDuty API'
