6ab4409a19ba7e72f7e1158f7eb23e38d465e038
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2015-11-10 14:58:18 -0800

    change modifiers to only work with additive modifiers

--- libraries/base.rb
+++ libraries/base.rb
@@ -27,7 +27,7 @@ class Nagios
                   :use
 
     def initialize
-      @modifiers = {}
+      @add_modifiers = {}
     end
 
     def merge!(obj)
@@ -175,7 +175,7 @@ class Nagios
       longest = get_longest_option(options)
       options.each do |k, v|
         k = k.to_s
-        v = (@modifiers[k] || '') + v.to_s
+        v = (@add_modifiers[k] || '') + v.to_s
         diff = longest - k.length
         r.push(k.rjust(k.length + 2) + v.rjust(v.length + diff + 2))
       end
@@ -278,9 +278,9 @@ class Nagios
     def update_hash_options(hash)
       hash.each do |k, v|
         push(Nagios::CustomOption.new(k.upcase, v)) if k.start_with?('_')
-        if v.is_a? String
-          @modifiers[k] = v[/^[+!]/]
-          v = v.gsub(/^[+!]/, '')
+        if v.is_a?(String) && v.start_with?('+')
+          @add_modifiers[k] = '+'
+          v.gsub!(/^\+/, '')
         end
         m = k + '='
         send(m, v) if self.respond_to?(m)
@@ -290,7 +290,7 @@ class Nagios
     def update_members(hash, option, object, remote = false)
       return if blank?(hash) || hash[option].nil?
       get_members(hash[option], object).each do |member|
-        n = Nagios.instance.find(object.new(member.gsub(/^[+!]/, '')))
+        n = Nagios.instance.find(object.new(member))
         push(n)
         n.push(self) if remote
       end
