105e559ca11926230357c9dd2e1be498de10b1b9
jaxjacobs <jack.jacobs@tealium.com>
2016-04-04 17:41:08 -0700

    Nagios updates

--- recipes/passive_crons.rb
+++ recipes/passive_crons.rb
@@ -17,122 +17,122 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-nagios_server = ""
-search = "role:#{node['nagios']['server_role']} AND domain:#{node[:domain]}*"
-search(:node, search) do |nagios_node|
-  Chef::Log.warn( "Found Nagios server for at #{nagios_node['ipaddress']}" )
-  nagios_server =  nagios_node['ipaddress']
-end
+#nagios_server = ""
+#search = "role:#{node['nagios']['server_role']} AND domain:#{node[:domain]}*"
+#search(:node, search) do |nagios_node|
+#  Chef::Log.warn( "Found Nagios server for at #{nagios_node['ipaddress']}" )
+#  nagios_server =  nagios_node['ipaddress']
+#end
 
-if node.roles.include?("hostname_eventstream") && node[:tealium][:use_nagios] == true
-  cron "Check Eventstream Logs" do
-    minute "*/15"
-    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_Eventstream_Logs.sh"
-    #minute "0"
-    #hour "20"
-    #day "1"
-    #month "11"
-    #command "/usr/lib/nagios/plugins/Check_Eventstream_Logs.sh"
-  end
+#if node.roles.include?("hostname_eventstream") && node[:tealium][:use_nagios] == true
+#  cron "Check Eventstream Logs" do
+#    minute "*/15"
+#    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_Eventstream_Logs.sh"
+#    #minute "0"
+#    #hour "20"
+#    #day "1"
+#    #month "11"
+#    #command "/usr/lib/nagios/plugins/Check_Eventstream_Logs.sh"
+#  end
+#
+#  template "/usr/lib/nagios/plugins/Check_Eventstream_Logs.sh" do
+#    source "check_eventstream_logs.sh.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#  end
+#
+#  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
+#    source "check_log3_passive.pl.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#    variables(
+#      :nagios_server => nagios_server
+#    )
+#  end
+#end
 
-  template "/usr/lib/nagios/plugins/Check_Eventstream_Logs.sh" do
-    source "check_eventstream_logs.sh.erb"
-    owner "root"
-    group "root"
-    mode 0755
-  end
+#if node.roles.include?("utui")
+#  cron "Check UTUI Logs" do
+#    minute "*/15"
+#    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_UTUI_Logs.sh"
+#  end
+#
+#  template "/usr/lib/nagios/plugins/Check_UTUI_Logs.sh" do
+#    source "check_utui_logs.sh.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#  end
+#
+#  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
+#    source "check_log3_passive.pl.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#    variables(
+#      :nagios_server => "10.250.168.137"
+#    )
+#  end
+#end
 
-  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
-    source "check_log3_passive.pl.erb"
-    owner "root"
-    group "root"
-    mode 0755
-    variables(
-      :nagios_server => nagios_server
-    )
-  end
-end
-
-if node.roles.include?("utui")
-  cron "Check UTUI Logs" do
-    minute "*/15"
-    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_UTUI_Logs.sh"
-  end
-
-  template "/usr/lib/nagios/plugins/Check_UTUI_Logs.sh" do
-    source "check_utui_logs.sh.erb"
-    owner "root"
-    group "root"
-    mode 0755
-  end
-
-  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
-    source "check_log3_passive.pl.erb"
-    owner "root"
-    group "root"
-    mode 0755
-    variables(
-      :nagios_server => "10.250.168.137"
-    )
-  end
-end
-
-if node.roles.include?("sitescan_sitemap") || node.roles.include?("sitescan_urls")
-  cron "Check SiteMap Logs" do
-    minute "*/15"
-    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_Sitemap_Logs.sh"
-  end
-
-  template "/usr/lib/nagios/plugins/Check_Sitemap_Logs.sh" do
-    source "check_sitemap_logs.sh.erb"
-    owner "root"
-    group "root"
-    mode 0755
-  end
-
-  region = node['ec2']['region']
-  nagios_server = "#{node["consul"]["server"][region]}"
-
-  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
-    source "check_log3_passive.pl.erb"
-    owner "root"
-    group "root"
-    mode 0755
-    variables(
-      :nagios_server => nagios_server
-    )
-  end
-end
+#if node.roles.include?("sitescan_sitemap") || node.roles.include?("sitescan_urls")
+#  cron "Check SiteMap Logs" do
+#    minute "*/15"
+#    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_Sitemap_Logs.sh"
+#  end
+#
+#  template "/usr/lib/nagios/plugins/Check_Sitemap_Logs.sh" do
+#    source "check_sitemap_logs.sh.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#  end
+#
+#  region = node['ec2']['region']
+#  nagios_server = "#{node["consul"]["server"][region]}"
+#
+#  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
+#    source "check_log3_passive.pl.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#    variables(
+#      :nagios_server => nagios_server
+#    )
+#  end
+#end
 
 
-if node.roles.include?("dc_uconnect")
-  cron "Check DC Uconnect Logs" do
-    minute "*/15"
-    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_DC_Uconnect_Logs.sh"
-    command "cat /dev/null"
-
-  end
-
-  template "/usr/lib/nagios/plugins/Check_DC_Uconnect_Logs.sh" do
-    source "check_dc_uconnect_logs.sh.erb"
-    owner "root"
-    group "root"
-    mode 0755
-  end
-
-  region = node['ec2']['region']
-  nagios_server = "#{node["consul"]["server"][region]}"
-
-  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
-    source "check_log3_passive.pl.erb"
-    owner "root"
-    group "root"
-    mode 0755
-    variables(
-      :nagios_server => nagios_server
-    )
-  end
-end
+#if node.roles.include?("dc_uconnect")
+#  cron "Check DC Uconnect Logs" do
+#    minute "*/15"
+#    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; /usr/lib/nagios/plugins/Check_DC_Uconnect_Logs.sh"
+#    command "cat /dev/null"
+#
+#  end
+#
+#  template "/usr/lib/nagios/plugins/Check_DC_Uconnect_Logs.sh" do
+#    source "check_dc_uconnect_logs.sh.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#  end
+#
+#  region = node['ec2']['region']
+#  nagios_server = "#{node["consul"]["server"][region]}"
+#
+#  template "/usr/lib/nagios/plugins/check_log3_passive.pl" do
+#    source "check_log3_passive.pl.erb"
+#    owner "root"
+#    group "root"
+#    mode 0755
+#    variables(
+#      :nagios_server => nagios_server
+#    )
+#  end
+#end
 
 
 
