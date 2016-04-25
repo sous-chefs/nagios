0be7aa6f349850fbef189adef2dbaf517053e373
jaxjacobs <jack.jacobs@tealium.com>
2016-04-25 12:36:15 -0700

    Removing passwords from cookbook part 2

--- files/default/plugins/queue-purge.sh
+++ files/default/plugins/queue-purge.sh
@@ -6,8 +6,8 @@ This script will unbind the http_requests_queue and purge the contents.
 MSG
 
 # delete binding
-rabbitmqadmin --vhost=/server2server --username=admin --password="toor4cars9window14five." delete binding source=events destination=http_requests_queue destination_type=queue properties_key=*.*.*.S.#
+rabbitmqadmin --vhost=/server2server --username=admin --password="<%= @adminpass %>" delete binding source=events destination=http_requests_queue destination_type=queue properties_key=*.*.*.S.#
 # purge queue
-rabbitmqadmin --vhost=/server2server --username=admin --password="toor4cars9window14five." purge queue name=http_requests_queue
+rabbitmqadmin --vhost=/server2server --username=admin --password="<%= @adminpass %>" purge queue name=http_requests_queue
 #delete queue
-rabbitmqadmin --vhost=/server2server --username=admin --password="toor4cars9window14five." delete queue name=http_requests_queue
+rabbitmqadmin --vhost=/server2server --username=admin --password="<%= @adminpass %>" delete queue name=http_requests_queue
