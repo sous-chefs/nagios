c6c63d402a2f0c7c61c68f12f904dd230b93a989
jaxjacobs <jack.jacobs@tealium.com>
2016-05-27 13:09:32 -0700

    Fixing some typos and omissions

--- files/default/plugins/check_rabbitmq_queue_count_new.sh
+++ files/default/plugins/check_rabbitmq_queue_count_new.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-tmp_file=/tmp/queues-$5.json
+tmp_file=/tmp/queues-$5-$6.json
 
 curl -i -u $3:$4 http://$1:15672/api/queues/%2fserver2server | tail -n+9 > $tmp_file
 
-python /usr/lib/nagios/plugins/check_rabbitmq_queue_count.py /tmp/queues-$5.json 2> /tmp/count-$5.log
+python /usr/lib/nagios/plugins/check_rabbitmq_queue_count.py /tmp/queues-$5-$6.json 2> /tmp/count-$5-$6.log
 
 queue_count=`cat $tmp_file | grep $6 | wc -l`
 
