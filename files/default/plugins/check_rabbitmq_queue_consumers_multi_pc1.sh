d205f6512180ffd9919592816ff28ba9824af8ea
jaxjacobs <jack.jacobs@tealium.com>
2016-08-16 12:15:43 -0700

    Some PC1 stuff for Nagios

---
+++ files/default/plugins/check_rabbitmq_queue_consumers_multi_pc1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+i=0
+tmp_file2=/tmp/testfilelog.log
+
+while [ $i -le $8 ]; do
+  queue=$6_$i
+
+  tmp_file=/tmp/consumers-$5-$queue.json
+
+  curl -i -u $3:$4 http://$1:15672/api/queues/%2fpc1_server2server/$queue | tail -n+9 > $tmp_file
+
+  if [[ ! -s $tmp_file ]]; then
+    echo "Not OK - Problem with queue" $queue"."
+    exit 2 ;
+  fi
+
+  python  /usr/lib/nagios/plugins/check_rabbitmq_queue_consumers.py $tmp_file 2> /tmp/consumer_count-$5-$queue.log
+
+  consumer_count=`cat $tmp_file`
+  if [ $consumer_count != $7 ]; then
+    echo "Not OK - Failed! Consumer count is" $consumer_count ", it should be" $7 "for queue" $queue"."
+
+    echo $i > /tmp/failed_queue.log
+
+    exit 2
+  fi
+
+  i=$(($i +1 ))
+done
+
+echo "OK - All" $6 "queues have" $7 "consumer/s."
+exit
---
+++ files/default/plugins/check_rabbitmq_queue_count_new_pc1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tmp_file=/tmp/queues-$5-$6.json
+
+curl -i -u $3:$4 http://$1:15672/api/queues/%2fpc1_server2server | tail -n+9 > $tmp_file
+
+python /usr/lib/nagios/plugins/check_rabbitmq_queue_count.py /tmp/queues-$5-$6.json 2> /tmp/count-$5-$6.log
+
+queue_count=`cat $tmp_file | grep $6 | wc -l`
+
+echo $6 "queue_count is" $queue_count". It should be at greater than or equal to" $7
+
+if (($queue_count < $7)); then
+  echo $6 "queue_count is" $queue_count". It should be at greater than or equal to" $7
+  exit 2
+fi
+
+exit
