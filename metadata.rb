6601a995171fb9d897e9ae18cec089fd2029ccb6
Tim Smith <tsmith@chef.io>
2017-01-26 14:16:28 -0800

    Update my e-mail address
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'nagios'
 maintainer        'Tim Smith'
-maintainer_email  'tsmith84@gmail.com'
+maintainer_email  'tsmith@chef.io'
 license           'Apache 2.0'
 description       'Installs and configures Nagios server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
