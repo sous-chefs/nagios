3bf7feeae0163f9c6271e727b66bb4cdda8d37ad
Stephen Hoekstra <shoekstra@schubergphilis.com>
2017-06-13 11:49:37 +0200

    Update metadata maintainer and repo urls

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name              'nagios'
-maintainer        'Tim Smith'
-maintainer_email  'tsmith@chef.io'
-license           'Apache-2.0'
-description       'Installs and configures Nagios server'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '7.2.7'
-issues_url       'https://github.com/schubergphilis/nagios/issues'
-source_url       'https://github.com/schubergphilis/nagios'
+name             'nagios'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Installs and configures Nagios server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '7.2.7'
+issues_url       'https://github.com/sous-chefs/nagios/issues'
+source_url       'https://github.com/sous-chefs/nagios'
 chef_version     '>= 12.1' if respond_to?(:chef_version)
 
 recipe 'default', 'Installs Nagios server.'
