7ffd57cb2f29d76d0f96b64e658e6c203f8542ec
dsiperek <dsiperek>
2017-11-21 14:29:23 -0800

    Removed ReadME and Changelog updates

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs and configures Nagios server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '8.0.1'
+version          '8.0.0'
 issues_url       'https://github.com/sous-chefs/nagios/issues'
 source_url       'https://github.com/sous-chefs/nagios'
 chef_version     '>= 12.9' if respond_to?(:chef_version)
