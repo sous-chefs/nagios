4ea669db6dec36b2b46265c21fe559927b249b43
Lance Albertson <lance@osuosl.org>
2021-01-29 16:43:37 -0800

    Use apache_dir helper method instead of attribute
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/server_package.rb
+++ recipes/server_package.rb
@@ -39,7 +39,7 @@ end
 package node['nagios']['server']['packages']
 
 # File typically exists on Debian
-file "#{node['apache']['dir']}/conf-enabled/#{node['nagios']['server']['vname']}-cgi.conf" do
+file "#{apache_dir}/conf-enabled/#{node['nagios']['server']['vname']}-cgi.conf" do
   manage_symlink_source true
   action :delete
 end
